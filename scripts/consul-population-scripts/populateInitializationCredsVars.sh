db042cf7c39c4cc2500503d5b70284fdd2d99061
Muhammad Balagamwala <bilal2206@gmail.com>
2018-10-05 12:33:04 -0700

    - Fixed bugs

--- scripts/consul-population-scripts/populateInitializationCredsVars.sh
+++ scripts/consul-population-scripts/populateInitializationCredsVars.sh
@@ -1,4 +1,4 @@
-consul kv put avi/credentials/controller 192.168.38.71
-consul kv put avi/credentials/username admin
-consul kv put avi/credentials/password Password123
-consul kv put avi/credentials/api_version 17.2.13
+consul kv put avi/controller 192.168.38.71
+consul kv put avi/username admin
+consul kv put avi/password Password123
+consul kv put avi/api_version 17.2.13
--- scripts/consul-templates/initializationCredsVars.tpl
+++ scripts/consul-templates/initializationCredsVars.tpl
@@ -2,5 +2,5 @@ avi_credentials:
   controller: {{ key "avi/controller/ip" }}
   username: '{{ key "avi/username" }}'
   password: '{{ key "avi/password" }}'
-  api_version: {{ key "avi/credentials/api_version" }}
+  api_version: {{ key "avi/api_version" }}
   

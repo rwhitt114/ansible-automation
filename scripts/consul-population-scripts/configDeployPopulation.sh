d6ba85a642eaca018fa6cf6eec5436e7b92461ea
Muhammad Balagamwala <bilal2206@gmail.com>
2018-10-10 10:53:07 -0700

    - Fixed directory structure in the population scripts

--- scripts/consul-population-scripts/configDeployPopulation.sh
+++ scripts/consul-population-scripts/configDeployPopulation.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-consul kv put avi/ovftool_path /usr/lib/vmware-ovftool
-consul kv put avi/vcenter/datacenter DC1
-consul kv put avi/vcenter/cluster MyCluster1
-consul kv put avi/vcenter/mgmt_network "VM Network"
-consul kv put avi/vcenter/ova_path /home/ge/controller-17.2.13-9168.ova
-consul kv put avi/vcenter/power_on true
-consul kv put avi/serviceenginegroup/vcenter/folder ge
-consul kv put avi/vcenter/number_of_cpus 8
-consul kv put avi/vcenter/memory 24
-consul kv put avi/controllers/1/name AviController1
-consul kv put avi/controllers/1/vcenter/host 192.168.38.20
-consul kv put avi/controllers/1/mgmt_ip 192.168.38.71
-consul kv put avi/controllers/1/mgmt_mask 255.255.255.0
-consul kv put avi/controllers/1/default_gw 192.168.38.1
-consul kv put avi/controllers/2/name 
-consul kv put avi/controllers/2/vcenter/host 
-consul kv put avi/controllers/2/mgmt_ip 
-consul kv put avi/controllers/2/mgmt_mask 
-consul kv put avi/controllers/2/default_gw 
-consul kv put avi/controllers/3/name 
-consul kv put avi/controllers/3/vcenter/host 
-consul kv put avi/controllers/3/mgmt_ip 
-consul kv put avi/controllers/3/mgmt_mask 
-consul kv put avi/controllers/3/default_gw 
+consul kv put properties/avi/config/ovftool_path /usr/lib/vmware-ovftool
+consul kv put properties/avi/config/vcenter/datacenter DC1
+consul kv put properties/avi/config/vcenter/cluster MyCluster1
+consul kv put properties/avi/config/vcenter/mgmt_network "VM Network"
+consul kv put properties/avi/config/vcenter/ova_path /home/ge/controller-17.2.13-9168.ova
+consul kv put properties/avi/config/vcenter/power_on true
+consul kv put properties/avi/config/vcenter/folder ge
+consul kv put properties/avi/config/vcenter/number_of_cpus 8
+consul kv put properties/avi/config/vcenter/memory 24
+consul kv put properties/avi/config/controllers/1/name AviController1
+consul kv put properties/avi/config/controllers/1/vcenter/host 192.168.38.20
+consul kv put properties/avi/config/controllers/1/mgmt_ip 192.168.38.71
+consul kv put properties/avi/config/controllers/1/mgmt_mask 255.255.255.0
+consul kv put properties/avi/config/controllers/1/default_gw 192.168.38.1
+consul kv put properties/avi/config/controllers/2/name 
+consul kv put properties/avi/config/controllers/2/vcenter/host 
+consul kv put properties/avi/config/controllers/2/mgmt_ip 
+consul kv put properties/avi/config/controllers/2/mgmt_mask 
+consul kv put properties/avi/config/controllers/2/default_gw 
+consul kv put properties/avi/config/controllers/3/name 
+consul kv put properties/avi/config/controllers/3/vcenter/host 
+consul kv put properties/avi/config/controllers/3/mgmt_ip 
+consul kv put properties/avi/config/controllers/3/mgmt_mask 
+consul kv put properties/avi/config/controllers/3/default_gw 
--- scripts/consul-population-scripts/configInitSePopulation.sh
+++ scripts/consul-population-scripts/configInitSePopulation.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-consul kv put avi/serviceenginegroup/name Default-Group
-consul kv put avi/serviceenginegroup/cloud_ref /api/cloud?name=Default-Cloud
-consul kv put avi/serviceenginegroup/vcenter/folder ge
-consul kv put avi/serviceenginegroup/vcenter/cluster/ref /api/vimgrclusterruntime?name=MyCluster1
-consul kv put avi/serviceenginegroup/vcenter/cluster/include True
-consul kv put avi/serviceenginegroup/max_se 4
-consul kv put avi/serviceenginegroup/buffer_se 1
-consul kv put avi/serviceenginegroup/se_name_prefix hpe
+consul kv put properties/avi/config/serviceenginegroup/name Default-Group
+consul kv put properties/avi/config/serviceenginegroup/cloud_ref /api/cloud?name=Default-Cloud
+consul kv put properties/avi/config/vcenter/folder ge
+consul kv put properties/avi/config/serviceenginegroup/vcenter/cluster/ref /api/vimgrclusterruntime?name=MyCluster1
+consul kv put properties/avi/config/serviceenginegroup/vcenter/cluster/include True
+consul kv put properties/avi/config/serviceenginegroup/max_se 4
+consul kv put properties/avi/config/serviceenginegroup/buffer_se 1
+consul kv put properties/avi/config/serviceenginegroup/se_name_prefix hpe
 
-consul kv put avi/network/name "VM Network"
-consul kv put avi/network/cloud_ref
-consul kv put avi/network/update_method PUT
-consul kv put avi/network/vcenter_dvs false
-consul kv put avi/network/dhcp_enabled false
+consul kv put properties/avi/config/vcenter/mgmt_network "VM Network"
+consul kv put properties/avi/config/network/cloud_ref
+consul kv put properties/avi/config/network/update_method PUT
+consul kv put properties/avi/config/network/vcenter_dvs false
+consul kv put properties/avi/config/network/dhcp_enabled false
 
-consul kv put avi/network/configured_subnets/prefix/ip_addr/addr 192.168.38.0
-consul kv put avi/network/configured_subnets/prefix/ip_addr/type V4
-consul kv put avi/network/configured_subnets/prefix/mask 24
+consul kv put properties/avi/config/network/configured_subnets/prefix/ip_addr/addr 192.168.38.0
+consul kv put properties/avi/config/network/configured_subnets/prefix/ip_addr/type V4
+consul kv put properties/avi/config/network/configured_subnets/prefix/mask 24
 
-consul kv put avi/network/configured_subnets/static_ranges/begin/addr 192.168.38.87
-consul kv put avi/network/configured_subnets/static_ranges/begin/type V4
+consul kv put properties/avi/config/network/configured_subnets/static_ranges/begin/addr 192.168.38.87
+consul kv put properties/avi/config/network/configured_subnets/static_ranges/begin/type V4
 
-consul kv put avi/network/configured_subnets/static_ranges/end/addr 192.168.38.90
-consul kv put avi/network/configured_subnets/static_ranges/end/type V4
+consul kv put properties/avi/config/network/configured_subnets/static_ranges/end/addr 192.168.38.90
+consul kv put properties/avi/config/network/configured_subnets/static_ranges/end/type V4
 
-consul kv put avi/cloud_discovery_wait 1
+consul kv put properties/avi/config/cloud_discovery_wait 1
 
-consul kv put avi/cloud/name Default-Cloud
-consul kv put avi/cloud/vtype CLOUD_VCENTER
-consul kv put avi/cloud/dhcp_enabled false
-consul kv put avi/cloud/license_type LIC_CORES
-consul kv put avi/vcenter/user administrator@mitchchen.com
-consul kv put avi/vcenter/password h6j5?HxL3zlSO$
-consul kv put avi/vcenter/datacenter DC1
-consul kv put avi/vcenter/dvs false
-consul kv put avi/vcenter/privilege WRITE_ACCESS
-consul kv put avi/vcenter/url 192.168.38.20
-consul kv put avi/vcenter/management_network "/api/vimgrnwruntime?name=VM Network"
-consul kv put avi/vcenter/management_ip_subnet/ip_addr/addr 192.168.38.0
-consul kv put avi/vcenter/management_ip_subnet/ip_addr/type V4
-consul kv put avi/vcenter/management_ip_subnet/mask 24
+consul kv put properties/avi/config/cloud/name Default-Cloud
+consul kv put properties/avi/config/cloud/vtype CLOUD_VCENTER
+consul kv put properties/avi/config/cloud/dhcp_enabled false
+consul kv put properties/avi/config/cloud/license_type LIC_CORES
+consul kv put properties/avi/config/vcenter/user administrator@mitchchen.com
+consul kv put properties/avi/config/vcenter/password h6j5?HxL3zlSO$
+consul kv put properties/avi/config/vcenter/datacenter DC1
+consul kv put properties/avi/config/vcenter/dvs false
+consul kv put properties/avi/config/vcenter/privilege WRITE_ACCESS
+consul kv put properties/avi/config/vcenter/url 192.168.38.20
+consul kv put properties/avi/config/vcenter/management_network "/api/vimgrnwruntime?name=VM Network"
+consul kv put properties/avi/config/vcenter/management_ip_subnet/ip_addr/addr 192.168.38.0
+consul kv put properties/avi/config/vcenter/management_ip_subnet/ip_addr/type V4
+consul kv put properties/avi/config/vcenter/management_ip_subnet/mask 24
         
         
 
